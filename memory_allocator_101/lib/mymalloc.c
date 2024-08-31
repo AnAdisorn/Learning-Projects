e7dbff65cf0f280b0fb219d10cb82514e7c4aa39
HeroAdisorn <adisornbkk@gmail.com>
2024-08-31 17:36:55 +0200

    add calloc and realloc

--- memory_allocator_101/lib/mymalloc.c
+++ memory_allocator_101/lib/mymalloc.c
@@ -73,36 +73,77 @@ void myfree(void *block)
     header_t *header, *tmp;
     void *programbreak;
 
-    if (!block)
+    if (!block) // no block, do nothing
         return;
 
     pthread_mutex_lock(&global_malloc_lock);
+    // get the header of the block
+    // by getting a pointer that is behind the block by a distance equalling the size of the header.
     header = (header_t *)block - 1;
 
     programbreak = sbrk(0);
+    // check if the block to be freed is at the end of the heap
     if ((char *)block + header->s.size == programbreak)
     {
         if (head == tail)
         {
             head = tail = NULL;
         }
-        else
+        else // while loop to set block to NULL, from head to tail
         {
             tmp = head;
             while (tmp)
             {
-                if (tmp->s.next == tail)
+                if (tmp->s.next == tail) // next one is tail
                 {
-                    tmp->s.next = NULL;
-                    tail = tmp;
+                    tmp->s.next = NULL; // set next to NULL, this will break while loop in the next loop
+                    tail = tmp;         // this tmp block is the new tail
                 }
-                tmp = tmp->s.next;
+                tmp = tmp->s.next; // move to next
             }
         }
-        sbrk(0 - sizeof(header_t) - header->s.size);
+        sbrk(0 - sizeof(header_t) - header->s.size); // release the memory
         pthread_mutex_unlock(&global_malloc_lock);
         return;
     }
     header->s.is_free = 1;
     pthread_mutex_unlock(&global_malloc_lock);
+}
+
+// allocate memory for an array of num elements of nsize bytes each and returns a pointer to the allocated memory
+void *mycalloc(size_t num, size_t nsize)
+{
+    size_t size;
+    void *block;
+    if (!num || !nsize)
+        return NULL;
+    size = num * nsize;
+    // check mul overflow
+    if (nsize != size / num)
+        return NULL;
+    block = mymalloc(size);
+    if (!block)
+        return NULL;
+    memset(block, 0, size); // clear the allocated memory to all zero
+    return block;
+}
+
+// changes the size of the given memory block to the given size
+void *myrealloc(void *block, size_t size)
+{
+    header_t *header;
+    void *ret;
+    if (!block || !size)
+        return malloc(size);
+    header = (header_t *)block - 1;
+    if (header->s.size >= size)
+        return block;
+    ret = malloc(size);
+    if (ret)
+    {
+
+        memcpy(ret, block, header->s.size); // relocate contents to the new bigger block
+        free(block);                        // free the old memory block
+    }
+    return ret;
 }
